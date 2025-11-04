699e1a2b0abb4faae16f70f44a357a594ccec672
Sanakata <155560381+Sanakata@users.noreply.github.com>
2025-11-04 11:07:38 +0800

    Create Super_Sale_C++.cpp

---
+++ Super_Sale_(Medium)/Super_Sale_C++.cpp
@@ -0,0 +1,27 @@
+c#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+/*
+Title: Super Sale
+Difficulty: Medium
+*/
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    string items, item; cin >> items;
+    vector<double> prices;
+    double price, ans = 0;
+    istringstream ss(items);
+    while(getline(ss, item, ',')){
+        price = stod(item);
+        prices.push_back(price);
+    }
+    sort(prices.begin(), prices.end(), greater<double>());
+    for(int i = 1; i < prices.size(); i++) ans += (prices[i] * 0.3);
+    ans *= 1.07;
+    cout << floor(ans) << "\n";
+}
