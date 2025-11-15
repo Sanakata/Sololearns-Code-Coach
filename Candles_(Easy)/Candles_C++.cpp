a289e3ae6f2d75b2cd888e5482457a12aba6cedc
Sanakata <155560381+Sanakata@users.noreply.github.com>
2025-11-15 12:50:17 +0800

    Create Candles_C++.cpp

---
+++ Candles_(Easy)/Candles_C++.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Title: Candles
+Difficulty: Easy
+*/
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int n; cin >> n;
+    cout << ++n * 9;
+}
