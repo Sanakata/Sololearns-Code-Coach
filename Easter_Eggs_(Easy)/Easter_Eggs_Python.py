f198f74830abef8e974af0bf5af8bc8ec007170a
Sanakata <155560381+Sanakata@users.noreply.github.com>
2025-11-19 17:32:33 +0800

    Add Easter Eggs Python script

---
+++ Easter_Eggs_(Easy)/Easter_Eggs_Python.py
@@ -0,0 +1,7 @@
+# Title: Easter Eggs
+# Difficulty: Easy
+
+total_eggs = int(input())
+your_eggs = int(input())
+friends_eggs = int(input())
+print("Candy Time" if total_eggs == your_eggs + friends_eggs else "Keep Hunting")
