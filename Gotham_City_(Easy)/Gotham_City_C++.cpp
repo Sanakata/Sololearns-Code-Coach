b1cd6e369b5be16071d7ff6a5703c64f0eb53667
Sanakata <155560381+Sanakata@users.noreply.github.com>
2025-11-21 02:56:45 +0800

    Create Gotham_City_C++.cpp

---
+++ Gotham_City_(Easy)/Gotham_City_C++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+/*
+Title: Gotham City
+Difficulty: Easy
+*/
+
+int main(){
+    long long c; cin >> c;
+    cout << (c > 10 ? "Good Luck out there!" : (c > 4 ? "Help me Batman" : "I got this!")) << "\n";
+}
