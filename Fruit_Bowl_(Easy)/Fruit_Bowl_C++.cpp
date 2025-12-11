20c46e4323a4f700da20a5101b5f55a3ad4ad47f
Sanakata <155560381+Sanakata@users.noreply.github.com>
2025-12-11 22:13:49 +0800

    Add Fruit Bowl C++ program

---
+++ Fruit_Bowl_(Easy)/Fruit_Bowl_C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+/*
+Title: Fruit Bowl
+Difficultyl: Easy
+*/
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int f; cin >> f;
+    cout << f / 6 << "\n";
+}
