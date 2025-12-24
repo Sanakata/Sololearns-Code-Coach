c8106bfca68153295554f42f58850a1b0e3819ab
Sanakata <155560381+Sanakata@users.noreply.github.com>
2025-12-24 14:09:49 +0800

    Create Neverland_C++.cpp

---
+++ Neverland_(Easy)/Neverland_C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+/*
+Title: Neverland
+Difficulty: Easy
+*/
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int a, b; cin >> a >> b;
+    cout << "My twin is " << a + b << " years old and they are " << b << " years older than me";
+}
