3114d563287afcc8705b7bb7a01499c609d08d7e
Sanakata <155560381+Sanakata@users.noreply.github.com>
2026-01-04 19:46:11 +0800

    Add Halloween Candy calculation program

---
+++ Halloween_Candy_(Easy)/Halloween_Candy_C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+/*
+Title: Halloween Candy
+Difficulty: Easy
+*/
+
+int main(){
+    int houses;
+    scanf("%d", &houses);
+    printf("%d", (int)ceil(200.0 / houses));
+}
