43cae795edbd647954bd0bc3a6ce3943f0ebb818
Sanakata <155560381+Sanakata@users.noreply.github.com>
2026-01-23 22:01:36 +0800

    Create Neverland_C.c

---
+++ Neverland_(Easy)/Neverland_C.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+Title: Neverland
+Difficulty: Easy
+*/
+
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("My twin is %d years old and they are %d years older than me", a + b, b);
+}
